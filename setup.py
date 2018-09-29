402dbb1572a045b4139b6ba70f6b207ffc37f0ea
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-09-29 19:53:13 +0100

    update setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
 from setuptools import setup
+import os
+
+
+def is_package(path):
+    return (os.path.isdir(path) and os.path.isfile(os.path.join(path, '__init__.py')))
+
+
+def find_packages(path, base="" ):
+    """ Find all packages in path """
+    packages = {}
+    for item in os.listdir(path):
+        dir = os.path.join(path, item)
+        if is_package( dir ):
+            if base:
+                module_name = "%(base)s.%(item)s" % vars()
+            else:
+                module_name = item
+            packages[module_name] = dir
+            packages.update(find_packages(dir, module_name))
+    return packages
 
 
 with open("README.md", "r") as fh:
@@ -6,7 +26,7 @@ with open("README.md", "r") as fh:
 
 
 setup(name='cg_predict_price',
-      version='0.1',
+      version='0.3',
       description='Car Giant Predict Price of cars',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -14,7 +34,7 @@ setup(name='cg_predict_price',
       author='Mihai Blebea',
       author_email='mihaiserban.blebea@gmail.com',
       license='MIT',
-      packages=find_packages(),
+      packages=find_packages('.'),
       install_requires=[
           'beautifulsoup4',
           'certifi',
